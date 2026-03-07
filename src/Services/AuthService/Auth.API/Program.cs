ebb91f0856fe97bc90e82855b15ffbf0ca3e1122
piyushpingle55 <pinglepiyush55555@gmail.com>
2026-03-07 18:50:29 +0530

    C_07March2026_Auth service changes

--- src/Services/AuthService/Auth.API/Program.cs
+++ src/Services/AuthService/Auth.API/Program.cs
@@ -1,9 +1,15 @@
+using System.IO.Compression;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+//register Db context 
+// builder.Services.AddDbContext<AuthDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
